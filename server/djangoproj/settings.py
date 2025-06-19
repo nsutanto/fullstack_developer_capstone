fb5457dba5b6cbd17b22c2a4410a6370d0c55a36
nsutanto <nicholas.sutanto@gmail.com>
2025-06-18 22:34:44 -0500

    Split

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -33,7 +33,9 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     'https://nicholassuta-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = [
-    'https://nicholassuta-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+    'https://nicholassuta-8000.'
+    'theianext-0-labs-prod-misc-tools-us-east-0.'
+    'proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],

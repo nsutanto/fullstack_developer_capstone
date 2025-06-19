da49b790f129a3acb92f291da2b51f499a89390d
nsutanto <nicholas.sutanto@gmail.com>
2025-06-18 22:55:27 -0500

    Settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,7 +31,9 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'https://nicholassuta-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+    'https://nicholassuta-8000.'
+    'theianext-0-labs-prod-misc-tools-us-east-0.'
+    'proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = [
     'https://nicholassuta-8000.'
     'theianext-0-labs-prod-misc-tools-us-east-0.'
@@ -100,7 +102,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.'
+        'UserAttributeSimilarityValidator',
     },
     {
         'NAME':

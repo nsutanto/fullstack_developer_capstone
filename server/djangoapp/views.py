097b36ddf5199379ed6e3cbb2c8134f196cd0d61
nsutanto <nicholas.sutanto@gmail.com>
2025-06-18 22:53:52 -0500

    Final clean views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -15,7 +15,8 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
 from .models import CarMake, CarModel
-from .restapis import get_request, analyze_review_sentiments, post_review
+from .restapis import (get_request, analyze_review_sentiments)
+# post_review
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -135,7 +136,7 @@ def get_dealer_details(request, dealer_id):
 
 def add_review(request):
     if not request.user.is_anonymous:
-        data = json.loads(request.body)
+        # data = json.loads(request.body)
         try:
             # response = post_review(data)
             return JsonResponse({"status": 200})

bc4fb2fe6960f035bedf223784d58ffb80aec42d
nsutanto <nicholas.sutanto@gmail.com>
2025-06-18 23:14:27 -0500

    Try this

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,9 +1,10 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
+# from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
+
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
@@ -29,13 +30,14 @@ class CarMake(models.Model):
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    car_make = models.ForeignKey(
+        CarMake,
+        on_delete=models.CASCADE)  # Many-to-One relationship
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
-        ('WAGON', 'Wagon'),
-        # Add more choices as required
+        ('WAGON', 'Wagon'), # Add more choices as required
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
@@ -47,5 +49,3 @@ class CarModel(models.Model):
 
     def __str__(self):
         return self.name  # Return the name as the string representation
-    
-

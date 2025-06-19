a819855591c6cba0e411e6db1dd49343e0e40ca6
nsutanto <nicholas.sutanto@gmail.com>
2025-06-18 22:52:20 -0500

    Push changes

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -134,12 +134,12 @@ def get_dealer_details(request, dealer_id):
 
 
 def add_review(request):
-    if (request.user.is_anonymous == False):
+    if not request.user.is_anonymous:
         data = json.loads(request.body)
         try:
-            response = post_review(data)
+            # response = post_review(data)
             return JsonResponse({"status": 200})
-        except:
+        except Exception:
             return JsonResponse({"status": 401,
                                  "message": "Error in posting review"})
     else:
